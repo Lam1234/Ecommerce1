1428f3e59fcfde861b4e4a2275b826bcdc42e4d0
Lam1234 <jialiang.lin86@gmail.com>
2017-06-02 14:08:34 +1000

    third commit

---
+++ public/js/checkout.js
@@ -0,0 +1,39 @@
+Stripe.setPublishableKey('pk_test_k1izWPPLzTxqavhIsoFz52ug');
+
+var $form = $('#checkout-form');
+
+$form.submit(function(event){
+	$('#charge-error').addClass('hidden');
+	$form.find('button').prop('disable',true);
+	Stripe.card.createToken({
+  number: $('#card-number').val(),
+  cvc: $('#card-cvc').val(),
+  exp_month: $('#card-expiry-month').val(),
+  exp_year: $('#card-expiry-year').val(),
+  name: $('#card-name').val() 
+}, stripeResponseHandler);
+
+	//wait a second, don't continue form submission
+	return false;
+
+});
+
+function stripeResponseHandler(status,response){
+
+	if(response.error){
+		$('#charge-error').removeClass('hidden');
+		$('#charge-error').text(response.error.message);
+		$form.find('button').prop('disable',false);
+	}else{
+		var token = response.id;
+		// Insert the token into the form so it gets submitted to the server:
+		$form.append($('<input type="hidden" name="stripeToken" />').val(token));
+
+    // Submit the form:
+    $form.get(0).submit();
+	}
+
+
+
+}
+
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,21 @@ Route::get('/shopping-cart',[
 	]);
 
 
+Route::get('/checkout',[
+	'uses' => 'ProductController@getCheckout',
+	'as' => 'checkout'
+
+	]);
+
+Route::post('/checkout',[
+	'uses' => 'ProductController@postCheckout',
+	'as' => 'checkout'
+
+
+
+	]);
+
+
 
 Route::group(['prefix'=>'user'],function(){
 
